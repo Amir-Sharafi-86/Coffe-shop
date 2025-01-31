f5c342a0791a75351d6cfd5bd60780fbef0e9395
Amir-Sharafi-86 <Amirsharafi8899@gmail.com>
2025-02-01 01:17:42 +0330

    addind HeaderDesktop && edit tailwindconfing

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode : "class",
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    
+    extend: {
+      
+      colors  :{
+        "brown" : "#4F2C19"
+      },
+      backgroundColor:{
+        "brown" :  "#E2DFD5"
+      },
+      fontFamily : {
+        "Dana" : "Dana",
+        "DanaMedium" : "Dana Medium",
+        "DanaDemibold" : "Dana Demibold",
+      },
+    },
+  },
+  plugins: [],
+}
+
